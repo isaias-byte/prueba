449ee7322b2108a63a96b2f784199518f202be45
Isaias Juarez <isaias.juarez6580@alumnos.udg.mx>
2020-09-22 12:35:09 -0500

    Implementamos la suma() y la resta()

---
+++ funciones.h
@@ -0,0 +1,15 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+#include <iostream>
+
+using namespace std;
+
+int suma(int a, int b){
+    return a+b;
+}
+
+int resta(int a, int b){
+    return a-b;
+}
+
+#endif // FUNCIONES_H
