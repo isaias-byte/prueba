d4204601edbce5bb22f69d87ce07fe338a9e148c
Isaias Juarez <isaias.juarez6580@alumnos.udg.mx>
2020-09-21 20:58:53 -0500

    Capturamos nombre y edad

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
+    string nombre;
+    int edad;
+    cout<<"Hola Mundo"<<endl;
 
+    cout<<"Dame el nombre: ";
+    getline(cin,nombre);
+    cout<<"Edad: ";
+    cin>>edad;
+    cout<<"El usuario se llama "<<nombre<<", y va a cumplir "<<(edad+1)<<endl;
     return 0;
 }
